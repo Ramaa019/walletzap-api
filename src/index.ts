5e92183d295a2e724b56e3f8c96bc1c37600dfaa
Rama <ramonpastre.com>
2026-08-16 17:49:40 +0000

    refactor: remove unused credentials:true from cors config

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || '*',
-    credentials: true,
+    origin: process.env.CORS_ORIGIN || '*'
   })
 );
 app.use(express.json({ limit: '10mb' }));
